170a971c4758a220b7e3fd022df8d62ed92a48d0
Bockinger <44947911+Bockinger@users.noreply.github.com>
2024-12-31 18:35:01 +0100

    css

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-
 let fruitDateLenght = 0;
-
+let dbFruit = [];
 
 function init() {
+  const openPopupBtn = document.getElementById('openPopup');
+  const closePopupBtn = document.getElementById('closePopup');
+  const popup = document.getElementById('popup');
+  const overlay = document.getElementById('overlay');
   fruitData();
-  render();
 }
-
 
 async function fruitData() {
   try {
     let response = await fetch('https://www.fruityvice.com/api/fruit/all');
-    let data = await response.json();
-    fruitDateLenght = data.length;
+    dbFruit = await response.json();
+    fruitDateLenght = dbFruit.length;
     statusBar(response);
-    dataExtra(data);
-    console.log(data);
-
+    dataExtra(dbFruit);
   } catch (error) {
     console.log("Fehler");
+    alert("Datenbankfehler !!");
   }
-
 }
-
 
 function statusBar(response) {
   document.getElementById('Container1').innerHTML = '<span>Status : ' + response.status + '</span>';
@@ -31,39 +29,52 @@ function statusBar(response) {
   document.getElementById('Container4').innerHTML = '<span>DB-Länge : ' + fruitDateLenght + '</span>';
 }
 
-
-
 function dataExtra(data) {
-
-  console.log("Datenläange ", fruitDateLenght);
-
   for (i = 0; i < fruitDateLenght; i++) {
     let id = data[i]["id"];
     let name = data[i]["name"];
-
-
-    render(id, name);
+    let calor = data[i]["nutritions"]["calories"];
+    let sugar = data[i]["nutritions"]["sugar"];
+    render(i, id, name, calor, sugar);
   }
-
-
 }
 
-
-function render(id, name) {
+function render(line, id, name, calor, sugar) {
   let element = document.getElementById('Fruittable');
-  element.innerHTML += template(id, name);
+  element.innerHTML += templateTable(line, id, name, calor, sugar)
 }
 
+function popuoClose() {
+  popup.style.display = 'none';
+  overlay.style.display = 'none';
+};
 
-
-function template(id, name) {
+function templateTable(line, id, name, calor, sugar) {
   return `
- <tr>
+ <tr class="TableRow" id="fruitLine${line}" onclick="lineEvaluation(${line})">
    <th scope="row">${id}</th>
    <td>${name}</td>
-    <td>Otto</td>
-    <td>@mdo</td>
+    <td>${calor}</td>
+    <td>${sugar}</td>
   </tr>
+ `
+}
 
-  `
+function templateInfoBox(id) {
+  return `
+  <br>
+  <br>
+  <span>Name : ${dbFruit[id]["name"]}</span><br>
+  <br>
+  <span>Kalorien : ${dbFruit[id]["nutritions"]["calories"]}</span><br>
+  <br>
+  <span>Zucker : ${dbFruit[id]["nutritions"]["sugar"]}</span>
+`
+}
+
+function lineEvaluation(id) {
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+  let element = document.getElementById('fruitInfoBox');
+  element.innerHTML = templateInfoBox(id);
 }
